f6f1f62091fc0feb93cd41dab18b14c78bc5af19
TK Vishal <vishalsanthosh1999@gmail.com>
2020-06-07 14:35:47 +0530

    deploy

--- client/src/utils/getPath.js
+++ client/src/utils/getPath.js
@@ -1,5 +1,5 @@
 export const getPath = (path) => {
   return process.env.NODE_ENV === "development"
     ? `http://localhost:8080${path}`
-    : `https://yata-brain.now.sh${path}`;
+    : `https://yata-brain.herokuapp.com${path}`;
 };
