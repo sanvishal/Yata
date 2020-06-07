94e36adc08482621df732a510f356c89caa2f7e2
TK Vishal <vishalsanthosh1999@gmail.com>
2020-06-07 15:04:25 +0530

    secret

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb://yatatkvishal:Vishal%4012345@ds149596.mlab.com:49596/yata-stash",
+  serverSecret:
+    "IknowwhatImustdo.Iwillshredthisuniversedowntoitslastatomandthen,withtheStonesyou'vecollectedforme,createanewoneteemingwithlifethatknowsnotwhatit'slost!",
+};
